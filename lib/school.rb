a5d2cf7c714a8873c6a6d324c8638bdbd5535074
Neil <zooming.in@hotmail.com>
2021-07-01 23:07:06 +1000

    updates

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+    def initialize(name)
+        self.roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] || = []
+        roster[grade] << student
+
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each do |grade, students|
+            roster[grade] = student.sort
+        end
+    end
+end
