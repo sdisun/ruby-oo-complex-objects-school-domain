25562f4d31157e327c4ef205b548968ec3e7d330
Neil <zooming.in@hotmail.com>
2021-07-16 01:14:21 +1000

    updates

--- lib/school.rb
+++ lib/school.rb
@@ -1,22 +1,21 @@
 # code here!
+
 class School
+    attr_accessor :roster
+    attr_reader :name
     def initialize(name)
-        self.roster = {}
+      @name = name
+      @roster = {}
     end
-
-    def add_student(student, grade)
-        roster[grade] || = []
-        roster[grade] << student
-
+    def add_student(name, grade)
+      @roster[grade] ?  @roster[grade] << name : @roster[grade] = [name]
     end
-
     def grade(grade)
-        roster[grade]
+      @roster[grade]
     end
-
-    def sort
-        roster.each do |grade, students|
-            roster[grade] = student.sort
-        end
+    def sort()
+      sorted_roster = {}
+      @roster.sort.each {|grade, roster| sorted_roster[grade] = roster.sort}
+      sorted_roster
     end
-end
+  end
